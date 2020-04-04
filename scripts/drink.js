7a5d8d9e84e758197dec109fc8b5774561fb211d
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-04 11:37:00 +0100

    HTML and main JS created. Now for the Jasmine Test.

---
+++ scripts/drink.js
@@ -0,0 +1,20 @@
+whatCanIDrink = function(age) {
+    if (age < 0) {
+        return "You're not born yet, or 'acting the maggot'.";
+    }
+    else if (age < 14) {
+            return "Drink Toddy";
+    }
+    else if (age < 18) {
+            return "Drink Coke";
+    }
+    else if (age < 21) {
+            "Drink Beer";
+    }
+    else if (age <  130) {
+            "Drink Whiskey";
+    }
+    else {
+        return "You're either too old to drink, or 'acting the maggot'.";
+    }
+};
